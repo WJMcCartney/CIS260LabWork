fc5331cc9edfdc3ea61e59747524c409e9ad454b
WJMcCartney <96458314+WJMcCartney@users.noreply.github.com>
2022-02-16 11:48:09 -0500

    Problem Three completed

---
+++ src/labThree/problemThreeSkippingNumbers.java
@@ -0,0 +1,24 @@
+package labThree;
+
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+public class problemThreeSkippingNumbers {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> takenInputs = new ArrayList<Integer>();
+		for (int i = 0; i < 20; i++) {
+			int counterI = i + 1;
+			String indexInput = JOptionPane.showInputDialog("Please type your " + counterI + " integer: ");
+			int indexInputInt = Integer.parseInt(indexInput);
+			takenInputs.add(indexInputInt);
+			if (indexInputInt == 2 || indexInputInt == 7 || indexInputInt == 10) {
+				continue;
+			}
+			System.out.println("The (" + counterI + ") input is " + takenInputs.get(i));
+		}
+
+	}
+
+}
