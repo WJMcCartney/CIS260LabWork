af9dd487252b441dc4b9b412fdf075ad14642f91
WJMcCartney <96458314+WJMcCartney@users.noreply.github.com>
2022-02-16 10:50:50 -0500

    lab 3 question one complete

---
+++ src/labThree/problemOneLoops.java
@@ -0,0 +1,26 @@
+package labThree;
+
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
+
+public class problemOneLoops {
+	public static void main(String args[]) {
+		int sum = 0;
+		ArrayList<Integer> takenInputs = new ArrayList<Integer>();
+		for (int i = 0; i < 10; i++) {
+			int counterI = i + 1;
+			String indexInput = JOptionPane.showInputDialog("Please type your " + counterI + " integer: ");
+			int indexInputInt = Integer.parseInt(indexInput);
+			takenInputs.add(indexInputInt);
+		}
+		for (int i = 0; i < 10; i++) {
+			sum = sum + takenInputs.get(i);
+			int averageOverall = sum / takenInputs.size();
+			if (i == 9) {
+				System.out.println(averageOverall);
+				System.exit(0);
+			}
+		}
+
+	}
+}
